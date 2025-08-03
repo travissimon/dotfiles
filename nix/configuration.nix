75e9eb644874bd6ab19fc42812f4cc9d89e1e835
tsimon <tsimon@gmail.com>
2025-08-03 11:42:06 +1000

    use latest kernel

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -14,6 +14,7 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
+    kernelPackages = pkgs.linuxPackages_latest;
     kernelModules = [ "i2c-dev" "i2c-piix4" ];
   };
 
